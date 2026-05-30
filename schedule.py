e601a55975cba37426abedfec515584980a38bd0
supper-misha <suppercrafter@mail.ru>
2026-05-30 22:50:49 +0300

    updated scheduler

--- schedule.py
+++ schedule.py
@@ -1,8 +1,8 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from routin import send_routin
+from stats import send_daily_prog
 
 scheduler = BackgroundScheduler()
-
 
 # scheduler.add_job(send_routin, 'cron', hour=12, minute=40, args=["morning"])
 
@@ -20,6 +20,13 @@ scheduler.add_job(
     hour=20,
     minute=5,
     args=["evening"],
+    timezone="Europe/Moscow"
+)
+scheduler.add_job(
+    send_daily_prog,
+    'cron',
+    hour=21,
+    minute=0,
     timezone="Europe/Moscow"
 )
 
